3aa6a8af4332444d5bfe4ce41de00f6491a650dc
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2020-02-15 10:37:57 +0500

    🔥🛠✔ update : added test for validating the JWT

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -1,7 +1,13 @@
-const user = require("../models/user");
-
-describe("generateAuthToken", () => {
+const { User } = require("../../../models/user");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const mongoose = require("mongoose");
+describe("user.generateAuthToken", () => {
 	it("Should return a Valid Json Token...!", () => {
-		// const result=
+		const payload = { _id: new mongoose.Types.ObjectId().toHexString(), isAdmin: true };
+		const user = new User(payload);
+		const token = user.generateAuthToken();
+		const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+		expect(decoded).toMatchObject(payload);
 	});
 });
